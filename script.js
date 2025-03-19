94ebfc7c1657f0f1fff7be6f0d7d177e983edc32
Phatisena <naphatism@hotmail.com>
2025-03-19 16:20:59 +0700

    Add files via upload
    Signed-off-by: Phatisena <naphatism@hotmail.com>

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function toggleMenu() {
     const sideBar = document.getElementById('side-bar');
+    const mainArea = document.getElementById('main-area');
     sideBar.classList.toggle('active');
+    mainArea.classList.toggle('active');
 }
 
 document.addEventListener('click', function(event) {
     const sideBar = document.getElementById('side-bar');
+    const mainArea = document.getElementById('main-area');
     const menuToggle = document.querySelector('.menu-toggle');
     if (!sideBar.contains(event.target) && !menuToggle.contains(event.target)) {
         sideBar.classList.remove('active');
+        mainArea.classList.remove('active');
     }
 });
